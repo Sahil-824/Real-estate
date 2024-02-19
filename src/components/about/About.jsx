8a4e26a6eed587d740c7a86f90a9c8f4f6dfcf09
Sahil Panchal <sahilpanchal824824@gmail.com>
2024-02-19 15:01:31 +0530

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import Back from "../common/Back"
+import Heading from "../common/Heading"
+import img from "../images/about.jpg"
+import "./about.css"
+
+const About = () => {
+  return (
+    <>
+      <section className='about'>
+        <Back name='About Us' title='About Us - Who We Are?' cover={img} />
+        <div className='container flex mtop'>
+          <div className='left row'>
+            <Heading title='Our Agency Story' subtitle='Check out our company story and work process' />
+
+         <p>
+
+
+Welcome to Rent-up, your premier destination for all your real estate needs.
+
+At Rent-up, we understand that finding the perfect property can be a daunting task. Whether you're searching for your dream home, a lucrative investment opportunity, or a cozy rental, our mission is to simplify the process and empower you to make informed decisions with confidence.</p>
+            <button className='btn2'>More About Us</button>
+          </div>
+          <div className='right row'>
+            <img src='./immio.jpg' alt='' />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default About
